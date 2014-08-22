f0a903a9744b746e07de4f86445f3de03c97c62d
Edward Paget <edward@cassetteta.pe>
2014-08-22 10:07:48 -0500

    Add Deps

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [liberator "0.12.0"] 
+                 [compojure "1.1.3"]
+                 [http-kit "2.1.18"]
+                 [clj-mesos "0.1.0-SNAPSHOT"]
+                 [org.apache.curator/curator-test "2.4.0"]
+                 [org.apache.curator/curator-x-discovery "2.4.0"]])
