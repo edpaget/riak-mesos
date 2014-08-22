9e9e956f3b8fcb18839077d6f89158df48baff90
gautam, prasanna <prasannagautam@gmail.com>
2014-08-22 10:53:09 -0500

    scaffolding first-pass

--- src/riak_mesos/core.clj
+++ src/riak_mesos/core.clj
@@ -1,6 +1,17 @@
-(ns riak-mesos.core)
+(ns riak-mesos.core
+  (:require [clj-mesos.scheduler :as sch]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def riak-scheduler (sch/scheduler
+                     (resource-offers [driver offers]
+                                      (let [[offer] offers]
+                                        (sch/launchTasks driver (:offer-id offer)
+                                                         [{:name "riak-task"
+                                                           :task-id "riak-01"
+                                                           :slave-id (:slave-id offer)
+                                                           :resources {:cpus 1.0
+                                                                       :mem 100.0}
+                                                           :command {:value "sleep 10"}}])))))
+
+(defn -main
+  [master]
+  #_(sch/run-scheduler master))
